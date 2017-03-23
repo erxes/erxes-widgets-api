5660098702cad02b7f4594448eb6b524c9c4bba1
Boldkhuu Batbaatar <hi@boldkhuu.com>
2017-03-23 22:43:24 +0800

    graphql server moved from erxes-widgets

---
+++ src/data/subscription-manager.js
@@ -0,0 +1,20 @@
+import { PubSub, SubscriptionManager } from 'graphql-subscriptions';
+import schema from './schema';
+
+const pubsub = new PubSub();
+
+const subscriptionManager = new SubscriptionManager({
+  schema,
+  pubsub,
+  setupFunctions: {
+    messageInserted: (options, args) => ({
+      newMessagesChannel: {
+        filter: message =>
+          message.conversationId === args.conversationId
+        ,
+      },
+    }),
+  },
+});
+
+export { subscriptionManager, pubsub };
---
+++ src/data/subscriptions.js
@@ -0,0 +1,7 @@
+export default {
+  Subscription: {
+    messageInserted(message) {
+      return message;
+    },
+  },
+};
