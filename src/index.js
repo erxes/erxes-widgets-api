5728d47558ab35001526606c222244674a01b486
Boldkhuu Batbaatar <hi@boldkhuu.com>
2017-03-29 15:36:49 +0900

    update deps

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ import { subscriptionManager } from './data/subscription-manager';
 import schema from './data/schema';
 import { markCustomerAsNotActive } from './data/utils';
 
-
 // load environment variables
 dotenv.config();
 
@@ -29,43 +28,41 @@ app.use(cors());
 app.use('/graphql', graphqlExpress({ schema }));
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
-const { GRAPHQL_PORT } = process.env;
-app.listen(GRAPHQL_PORT, () => {
-  console.log(`GraphQL server is running on port ${GRAPHQL_PORT}`);
-});
+// server
+const server = createServer(app);
+const { PORT } = process.env;
+const SUBSCRIPTION_PATH = '/subscriptions';
 
-// websocket server
-const httpServer = createServer((request, response) => {
-  response.writeHead(404);
-  response.end();
-});
-const { WS_PORT } = process.env;
-httpServer.listen(WS_PORT, () => {
-  console.log(`Websocket server is running on port ${WS_PORT}`);
-});
+server.listen(PORT, () => {
+  console.log(`GraphQL server is running on port ${PORT}`);
+  console.log(`Websocket server is running on port ${PORT}${SUBSCRIPTION_PATH}`);
 
-// subscription server
-const server = new SubscriptionServer( // eslint-disable-line no-unused-vars
-  { subscriptionManager },
-  httpServer,
-);
+  // subscription server
+  const subscriptionServer = new SubscriptionServer(
+    {
+      subscriptionManager,
+      onConnect(connectionParams, webSocket) {
+        webSocket.on('message', (message) => {
+          const parsedMessage = JSON.parse(message);
 
-// receive inAppConnected message and save integrationId, customerId in connection
-server.wsServer.on('connect', (connection) => {
-  connection.on('message', (message) => {
-    const parsedMessage = JSON.parse(message.utf8Data);
+          console.log(parsedMessage);
 
-    if (parsedMessage.type === 'inAppConnected') {
-      connection.inAppData = parsedMessage.value; // eslint-disable-line no-param-reassign
-    }
-  });
-});
+          if (parsedMessage.type === 'inAppConnected') {
+            webSocket.inAppData = parsedMessage.value; // eslint-disable-line no-param-reassign
+          }
+        });
+      },
+      onDisconnect(webSocket) {
+        const inAppData = webSocket.inAppData;
 
-// mark customer as not active when connection close
-server.wsServer.on('close', (connection) => {
-  const inAppData = connection.inAppData;
-
-  if (inAppData) {
-    markCustomerAsNotActive(inAppData.customerId);
-  }
+        if (inAppData) {
+          markCustomerAsNotActive(inAppData.customerId);
+        }
+      },
+    },
+    {
+      server,
+      path: SUBSCRIPTION_PATH,
+    },
+  );
 });
