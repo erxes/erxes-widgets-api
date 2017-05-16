6ec1940e0d8566a546975799bbdbd91a653e0fab
batamar <batamar.b@nmtec.co>
2017-05-16 17:45:20 +0800

    added onlineHours check

--- src/data/check-availability.js
+++ src/data/check-availability.js
@@ -63,28 +63,29 @@ export const checkAvailability = (integration, date) => {
   // auto ====================
 
   // check by everyday config
-  const everydayConf = integration.onlineHours.find(c => c.day === 'everyday');
+  const onlineHours = onlineHours || [];
+  const everydayConf = onlineHours.find(c => c.day === 'everyday');
 
   if (everydayConf) {
     return isDateInRange(date, everydayConf.from, everydayConf.to);
   }
 
   // check by weekdays config
-  const weekdaysConf = integration.onlineHours.find(c => c.day === 'weekdays');
+  const weekdaysConf = onlineHours.find(c => c.day === 'weekdays');
 
   if (weekdaysConf && isWeekday(day)) {
     return isDateInRange(date, weekdaysConf.from, weekdaysConf.to);
   }
 
   // check by weekends config
-  const weekendsConf = integration.onlineHours.find(c => c.day === 'weekends');
+  const weekendsConf = onlineHours.find(c => c.day === 'weekends');
 
   if (weekendsConf && isWeekend(day)) {
     return isDateInRange(date, weekendsConf.from, weekendsConf.to);
   }
 
   // check by regular day config
-  const dayConf = integration.onlineHours.find(c => c.day === day);
+  const dayConf = onlineHours.find(c => c.day === day);
 
   if (dayConf) {
     return isDateInRange(date, dayConf.from, dayConf.to);
