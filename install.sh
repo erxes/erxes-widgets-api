628a1f648cc36d47c858d42e82459556b14b39b9
Munkhbold <munkhbold.d@nmtec.co>
2017-11-29 12:40:50 +0800

    update shell installation branch

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ if [ ! -d .git ]; then
   echo 'Clone erxes repository and install its dependencies:'
   git clone https://github.com/erxes/erxes-api
   cd erxes-api
+  git checkout feature-company
   yarn install
 fi
 
