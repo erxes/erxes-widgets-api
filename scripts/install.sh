ae2ff2489b56bf0e6a6aa344ae35e83363e7bfd5
Munkhbold <munkhbold.d@nmtec.co>
2017-12-01 13:57:04 +0800

    add introduction in installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,3 +11,23 @@ fi
 #### 3. Create configuration. We use [dotenv](https://github.com/motdotla/dotenv) for this.
 
 cp .env.sample .env
+
+cat <<"EOF"
+ -------------------------------------------------------------------------
+ To get started:
+
+ Run erxes which is an AI meets open source messaging platform for sales and marketing
+ $ cd erxes/ && yarn start
+
+ Run api which for erxes administration apps:
+ $ cd erxes-app-api/ && yarn dev
+
+ Run redis server
+ $ redis-server
+
+ Run widget which embedable widget scripts server for erxes
+ $ cd erxes-widget/ && yarn dev
+
+ Run erxes apps which graphQL API for erxes apps
+ $ cd erxes-api/ && yarn dev
+EOF
