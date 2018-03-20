82a218c9226e3694cbb39a5db0bd2fee240c484a
batamar <batamar.b@nmtec.co>
2018-03-20 21:45:47 +0800

    Little fix

--- src/data/resolvers/mutations/messenger.js
+++ src/data/resolvers/mutations/messenger.js
@@ -140,7 +140,7 @@ export default {
     );
 
     // mark customer as active
-    await Customers.markCustomerAsActive(customerId);
+    await Customers.markCustomerAsActive(conversation.customerId);
 
     // notify app api
     mutateAppApi(`
