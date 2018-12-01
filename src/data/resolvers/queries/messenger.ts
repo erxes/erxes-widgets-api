ac411df3d9f275e189af2a448750a3244ae1b3ff
batamar <batamar.b@nmtec.co>
2018-12-01 16:40:40 +0800

    Added _id field to conversationDetail query

--- src/data/resolvers/queries/messenger.ts
+++ src/data/resolvers/queries/messenger.ts
@@ -77,6 +77,7 @@ export default {
     }
 
     return {
+      _id,
       messages: await Conversations.getMessages(conversation._id),
       isOnline: await isMessengerOnline(integrationId),
       participatedUsers: await Users.find({
--- src/data/schema.ts
+++ src/data/schema.ts
@@ -136,6 +136,7 @@ export const types = `
   }
 
   type ConversationDetailResponse {
+    _id: String
     messages: [ConversationMessage]
     participatedUsers: [User]
     isOnline: Boolean
