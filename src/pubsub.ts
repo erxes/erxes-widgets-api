4b0c0b5dc71b4bad8884c0391f986744745bfee6
batamar <batamar.b@nmtec.co>
2019-07-23 20:54:21 +0800

    add cote

--- src/pubsub.ts
+++ src/pubsub.ts
@@ -1,23 +1,8 @@
-import { GooglePubSub } from '@axelspringer/graphql-google-pubsub';
+import * as cote from 'cote';
 import * as dotenv from 'dotenv';
-import * as fs from 'fs';
-import * as Redis from 'ioredis';
-import * as path from 'path';
 
 // load environment variables
 dotenv.config();
-
-interface IPubSub {
-  publish(trigger: string, payload: any, options?: any): any;
-}
-
-interface IGoogleOptions {
-  projectId: string;
-  credentials: {
-    client_email: string;
-    private_key: string;
-  };
-}
 
 interface IPubsubData {
   type?: string;
@@ -25,70 +10,13 @@ interface IPubsubData {
   payload: any;
 }
 
-const {
-  PUBSUB_TYPE,
-  REDIS_HOST = 'localhost',
-  REDIS_PORT = 6379,
-  REDIS_PASSWORD = '',
-}: {
-  PUBSUB_TYPE?: string;
-  REDIS_HOST?: string;
-  REDIS_PORT?: number;
-  REDIS_PASSWORD?: string;
-} = process.env;
-
-// Docs on the different redis options
-// https://github.com/NodeRedis/node_redis#options-object-properties
-const redisOptions = {
-  host: REDIS_HOST,
-  port: REDIS_PORT,
-  password: REDIS_PASSWORD,
-  connect_timeout: 15000,
-  enable_offline_queue: true,
-  retry_unfulfilled_commands: true,
-  retry_strategy: options => {
-    // reconnect after
-    return Math.max(options.attempt * 100, 3000);
+const publisher = new cote.Publisher(
+  { name: 'erxes-widgets-api' },
+  {
+    log: false,
+    statusLogsEnabled: false,
   },
-};
-
-const configGooglePubsub = (): IGoogleOptions => {
-  const checkHasConfigFile = fs.existsSync(path.join(__dirname, '..', '/google_cred.json'));
-
-  if (!checkHasConfigFile) {
-    throw new Error('Google credentials file not found!');
-  }
-
-  const serviceAccount = require('../google_cred.json');
-
-  return {
-    projectId: serviceAccount.project_id,
-    credentials: {
-      client_email: serviceAccount.client_email,
-      private_key: serviceAccount.private_key,
-    },
-  };
-};
-
-const createPubsubInstance = (): IPubSub => {
-  let pubsub;
-
-  if (PUBSUB_TYPE === 'GOOGLE') {
-    const googleOptions = configGooglePubsub();
-
-    const googlePubsub = new GooglePubSub(googleOptions);
-
-    pubsub = googlePubsub;
-  } else {
-    const redisPubsub = new Redis(redisOptions);
-
-    pubsub = redisPubsub;
-  }
-
-  return pubsub;
-};
-
-const pubsubInstance = createPubsubInstance();
+);
 
 export const publish = (action: string, data: IPubsubData) => {
   const { NODE_ENV } = process.env;
@@ -97,5 +25,5 @@ export const publish = (action: string, data: IPubsubData) => {
     return;
   }
 
-  return pubsubInstance.publish('widgetNotification', JSON.stringify({ action, data }));
+  return publisher.publish('widgetNotification', JSON.stringify({ action, data }));
 };
